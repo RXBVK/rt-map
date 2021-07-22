053341836d6a09808ddbf4ecd969ccca5d0e18e2
RXBVK <52883785+RXBVK@users.noreply.github.com>
2021-07-22 16:00:20 +0200

    Delete .history directory

--- .history/src/components/CsvUploader/CsvUploader_20210721124512.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CsvUploader() {
-  return (
-    <div>
-      <input type="file" />
-    </div>
-  );
-}
--- .history/src/components/CsvUploader/CsvUploader_20210721125916.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CsvUploader() {
-  return <div className="dropzone"></div>;
-}
